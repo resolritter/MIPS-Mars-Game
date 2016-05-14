d17bd93c8941c966f8fb590c8e60d727af77cf5e
resolritter <17429390+resolritter@users.noreply.github.com>
2016-05-14 13:58:02 -0300

    Add files via upload

---
+++ game_7.asm
@@ -0,0 +1,328 @@
+.data
+a1: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+a2: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+a3: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a4: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a5: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a6: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a7: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a8: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+a9: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 3 0 3 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+b1: 1 1 0 0 0 0 0 0 0 0 0 1 1 3 0 3 0 3 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+b2: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+b3: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+b4: 1 1 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 1 1
+b5: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+b6: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+b7: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+b8: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+b9: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c1: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 2 2 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c2: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c3: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c4: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c5: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c6: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c7: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c8: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+c9: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+d1: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+d2: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+d3: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+d4: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+d5: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+aa1: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+aa2: 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+aa3: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+aa4: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+aa5: 1 1 1 1 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+aa6: 1 1 0 1 0 0 0 1 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+aa7: 1 1 0 1 0 0 1 1 0 0 0 0 0 0 1 0 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1 1
+aa8: 1 1 0 1 0 0 0 0 0 0 0 1 0 0 1 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 1 1
+aa9: 1 1 0 1 0 0 0 0 0 0 1 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1
+bb1: 1 1 0 1 1 1 1 0 0 1 1 1 0 0 0 0 1 0 0 1 1 1 1 1 1 1 1 1 0 0 1 1
+bb2: 1 1 0 1 0 0 0 0 0 0 0 1 1 1 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1
+bb3: 1 1 0 1 0 0 0 0 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1 
+bb4: 1 1 0 1 1 1 1 0 0 0 0 1 0 0 0 1 1 1 1 1 1 1 0 0 0 0 0 1 0 0 1 1
+bb5: 1 1 0 1 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1
+bb6: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 1 0 0 1 1
+bb7: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 0 0 0 0 0 1 0 0 1 1
+bb8: 1 1 1 1 1 1 1 1 1 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 0 0 1 1
+bb9: 1 1 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1 1 1 1 1 1 0 0 1 0 0 1 0 0 1 1
+cc1: 1 1 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 1
+cc2: 1 1 0 0 0 0 0 1 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 0 0 1 1
+cc3: 1 1 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 0 1 0 0 1 1
+cc4: 1 1 0 0 1 0 0 0 1 0 1 0 0 1 1 1 0 1 0 0 1 1 0 0 1 0 0 1 0 0 1 1
+cc5: 1 1 0 0 1 1 0 0 0 0 1 0 0 1 1 1 0 1 0 0 1 1 0 0 1 0 0 1 0 0 1 1
+cc6: 1 1 0 0 1 1 0 0 0 0 1 0 0 1 1 1 0 1 0 0 1 1 0 0 0 0 0 1 0 0 1 1
+cc7: 1 1 0 0 1 1 1 1 0 0 1 0 0 1 1 1 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1
+cc8: 1 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1
+cc9: 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 0 0 1 1
+dd1: 1 1 1 1 0 0 0 0 1 1 1 1 1 1 1 1 1 1 1 1 1 1 0 4 4 4 0 0 0 0 1 1
+dd2: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 4 0 4 4 4 4 0 1 1
+dd3: 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 4 4 4 0 0 4 0 1 1
+dd4: 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+dd5: 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
+vcganhouc: .asciiz "\n"
+vcganhou: .asciiz "Você ganhou! Parabéns!!!"
+
+.text
+
+addi $s1, $0, 1024
+
+#T9 DEFINE O LUGAR INICIAL DO JOGADOR
+addi $t9, $0, 268503228
+
+#DEFININDO A COR CINZA
+lui $t4, 0x00d3
+ori $t4, $t4, 0x0000d3d3
+
+#DEFININDO A COR VERDE
+ori $t5, $t5, 0x0000ff00
+
+#DEFININDO A COR AMARELO
+lui $t6, 0x00ff
+ori $t6, $t6, 0x0000ff00
+
+#DEFININDO A COR VERMELHO
+lui $t3, 0x00ff
+
+#DEFININDO LUGAR INICIAL DA COR VERDE
+ori $sp, $t0, 0x000008B8
+
+
+#DEFININDO O MAPA DO JOGO
+loop:
+la $s5, aa1
+bge $t9, $s5, secondmap
+jal firstmap
+aftermapset:
+lui $t1, 0x1004
+add $t2, $0, $0
+
+#MOVIMENTACAO
+#PARA BAIXO, ESQUERDA, DIREITA, CIMA
+
+addi $t8, $0, 50
+lui $t7, 0xffff
+addi $t7, $t7, 4
+lw $2, ($t7)
+sw $0, ($t7)
+beq $t8, $2, movedown
+addi $t8, $0, 52
+beq $t8, $2, moveleft
+addi $t8, $0, 54
+beq $t8, $2, moveright
+addi $t8, $0, 56
+beq $t8, $2, moveup
+
+#FIM MOVIMENTACAO
+
+#PEGAR CHAVE
+addi $t8, $0, 53
+beq $t8, $2, getkey
+
+#FIM PEGAR CHAVE
+
+innerloop:
+slt $s0, $t2, $s1
+beqz $s0, sai
+lw $s5, ($t0)
+beqz $s5, storegrey
+addi $t8, $0, 2
+beq $t8, $s5, storegreen
+addi $t8, $0, 3
+beq $t8, $s5, storeyellow
+addi $t8, $0, 4
+beq $t8, $s5, storered
+j storeblack
+
+firstmap:
+lui $t0, 0x1001
+jr $ra
+
+secondmap:
+lui $t0, 0x1001
+ori $t0, 0x00001000
+j aftermapset
+
+storeblack:
+sw $0, ($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+addi $t2, $t2, 1
+j innerloop
+
+storegrey:
+sw $t4, ($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+addi $t2, $t2, 1
+j innerloop
+
+storegreen:
+sw $t5, ($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+addi $t2, $t2, 1
+j innerloop
+
+storeyellow:
+sw $t6, ($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+addi $t2, $t2, 1
+j innerloop
+
+storered:
+sw $t3, ($t1)
+addi $t0, $t0, 4
+addi $t1, $t1, 4
+addi $t2, $t2, 1
+j innerloop
+
+movedown:
+addi $t9, $t9, 256
+lw $2, ($t9)
+subu $t9, $t9, 256
+bnez $2, loop
+addi $t9, $t9, 260
+lw $2, ($t9)
+subu $t9, $t9, 260
+bnez $2, loop
+addi $t8, $0, 2
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 252
+sw $t8, ($t9)
+addi $t9, $t9, 4
+sw $t8, ($t9)
+subu $t9, $t9, 132
+j innerloop
+
+moveleft:
+subu $t9, $t9, 4
+lw $s5, ($t9)
+addi $t9, $t9, 4
+bnez $s5, loop
+addi $t9, $t9, 124
+lw $s5, ($t9)
+subu $t9, $t9, 124
+bnez $s5, loop
+addi $t8, $0, 2
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 128
+sw $0, ($t9)
+subu $t9, $t9, 136
+sw $t8, ($t9)
+addi $t9, $t9, 128
+sw $t8, ($t9)
+subu $t9, $t9, 128
+j innerloop
+
+moveright:
+addi $t9, $t9, 8
+lw $s5, ($t9)
+subu $t9, $t9, 8
+bnez $s5, loop
+addi $t9, $t9, 136
+lw $s5, ($t9)
+subu $t9, $t9, 136
+bnez $s5, loop
+addi $t8, $0, 2
+sw $0, ($t9)
+addi $t9, $t9, 8
+sw $t8, ($t9)
+addi $t9, $t9, 120
+sw $0, ($t9)
+addi $t9, $t9, 8
+sw $t8, ($t9)
+subu $t9, $t9, 132
+j innerloop
+
+moveup:
+jal tryunlock
+aftermoveup:
+subu $t9, $t9, 128
+lw $s5, ($t9)
+addi $t9, $t9, 128
+bnez $s5, loop
+subu $t9, $t9, 124
+lw $s5, ($t9)
+addi $t9, $t9, 124
+bnez $s5, loop
+addi $t8, $0, 2
+addi $t9, $t9, 128
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+subu $t9, $t9, 260
+sw $t8, ($t9)
+addi $t9, $t9, 4
+sw $t8, ($t9)
+subu $t9, $t9, 4
+j innerloop
+
+showpos:
+add $a0, $0, $t9
+li $v0, 1
+syscall
+jr $ra
+
+getkey:
+bleu $t9, 268508755, innerloop
+bgeu $t9, 268508757, innerloop
+addi $t9, $t9, 8
+sw $0, ($t9)
+subu $t9, $t9, 128
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 128
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 108
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 4
+sw $0, ($t9)
+addi $t9, $t9, 12
+sw $0, ($t9)
+addi $t9, $0, 268508756
+add $t5, $0, $t3
+j innerloop
+
+tryunlock:
+bne $t3, $t5, aftermoveup
+bltu $t9, 268502324, aftermoveup
+bgtu $t9, 268502344, aftermoveup
+add $s5, $t9, $0
+addi $t9, $0, 268502072
+sw $0, ($t9)
+addi $t9, $t9, 8
+sw $0, ($t9)
+addi $t9, $t9, 116
+sw $0, ($t9)
+addi $t9, $t9, 8
+sw $0, ($t9)
+addi $t9, $t9, 8
+sw $0, ($t9)
+add $t9, $s5, $0
+la $a0, vcganhou
+li $a1, 1
+li $0, 55
+syscall
+j innerloop
+
+sai: j loop
+ganhou: nop
